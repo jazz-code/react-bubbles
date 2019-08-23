11b550cc1018a852211cd143f07a9d3c2cba348f
ant <ren.meta@gmail.com>
2019-08-23 11:32:46 -0400

    login page successfully redirects to bubble-page

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        <PrivateRoute exact path="/protected" component={BubblePage}/>
+        <PrivateRoute exact path="/bubble-page" component={BubblePage}/>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
